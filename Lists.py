46d143f2ee29f36b673cd5f9b1676602a7a0f3e7
Sparsh Gautam <69500031+SparshGautam@users.noreply.github.com>
2020-08-12 01:17:44 -0700

    Lists.py

---
+++ Lists.py
@@ -0,0 +1,15 @@
+list = ("apple","mango","banana","orange","papaya")  # A list is usually a set of words writeen under a curly bracket her 's' is a list
+
+print(list)                                          # now your list will be printed
+
+list2 = ("car","headphone","bike","aeroplane","mobile")
+
+print(list2[1])                                      # you can also print a single object here headphone will be printed as it is 2nd option
+
+names = ("john","james","jonath")                    #suppose you forget to add something you can use input function
+print(names)
+
+m = input(names)
+print(m)
+
+
