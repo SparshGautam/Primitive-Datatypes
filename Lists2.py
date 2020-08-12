78b964aa58d274ac227b46fa457dc59f11908adb
Sparsh Gautam <69500031+SparshGautam@users.noreply.github.com>
2020-08-12 10:34:05 -0700

    Lists2.py

---
+++ Lists2.py
@@ -0,0 +1,11 @@
+digits  = ("apple","microsoft","google","Tata")    # assigning list to digits
+
+print(digits[-1])                                  # as +ve is opposite of-ve here -1 represents to get output from backside of our list here "Tata" will be printed
+
+print(digits[:3])                                  # here you can use colon so that the first 3 lis items will be printed
+
+names = "hello my name is adarsh"                  # in the same way  you can use colon to select in range
+
+print(names[:5])                                   # here hello = 5 alphabets so i typed here 5
+
+print(digits[0:3])                                 #this function is known as strides here your 3rd digits will be printed leaving other 2
