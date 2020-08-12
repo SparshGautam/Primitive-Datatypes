f72bab707761d966b6713cfe2111df3fa61d7771
Sparsh Gautam <69500031+SparshGautam@users.noreply.github.com>
2020-08-11 22:57:11 -0700

    Update dataTP.py

--- dataTP.py
+++ dataTP.py
@@ -1,2 +1,5 @@
-# Primitive-Datatypes
-here we will learn about datatypes
+#THE FOLLOWING CODE MUST BE RUN IN THE CONSOLE ITSELF
+
+float(1)   #here we want integer 1 to be converted in float
+str(1)     #here we want integer 1 to be converted in string
+int("1")   #here we want string 1 to be converted in integer
