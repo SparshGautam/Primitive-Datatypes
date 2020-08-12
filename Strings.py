385621b1704770bdd5481120db8c54aedd941d57
Sparsh Gautam <69500031+SparshGautam@users.noreply.github.com>
2020-08-12 00:32:07 -0700

    Strings.py

---
+++ Strings.py
@@ -0,0 +1,6 @@
+s = "my name is anthony gonsalwis"  #string is anything written under "..."(qotations)
+
+s2 = 'my name is lala raja'         #it will work same if you use '...' (single quotations too)
+
+print(s)
+print(s2)
