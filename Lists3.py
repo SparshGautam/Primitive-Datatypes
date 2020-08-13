6c716c3151c458c631caaaefb6abd553449fe725
Sparsh Gautam <69500031+SparshGautam@users.noreply.github.com>
2020-08-13 04:30:22 -0700

    Lists3.py

---
+++ Lists3.py
@@ -0,0 +1,19 @@
+s= {"banana","blueberry","respberry"}           # assigning some value to s
+
+s.add("strawberry")                             # here i have typed this code under curly brackets you can use this function add
+
+print(s)                                        # now you can add anything get started
+
+n = {1,2,2,3,3,4,4,5,5,6,6,7,7,8,89,9,9}        # assigning value to n
+
+no = set(n)                                     # you can use this function to deny duplicate numbers
+
+print(no)                                       # lets print no
+
+lib1 = {'hary potter','hunger games','romeo'}   # assigning value to lib1
+
+lib2 = {'james','hary potter','adarsh'}         # assigning value to lib2
+
+m = lib1.intersection(lib2)                     # you can use intersection to find common in both terms here 
+
+print(m)                                        # we have hve harry potter both common so harry potter will be printed
