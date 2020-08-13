e6e2f0a3bbd486a0ca08c4d4f9b9624a2aeca964
Sparsh Gautam <69500031+SparshGautam@users.noreply.github.com>
2020-08-12 20:37:48 -0700

    ForLoop.py

---
+++ ForLoop.py
@@ -0,0 +1,6 @@
+digits = (1,2,3,4,5,6,7,8,9)      #assigning few numbers to digits
+
+print(digits[0:5])                # the colon indicates to print number in range 5
+
+for i in range(len(digits)):      # for loop to print in range to variable i
+  print(digits[0:i])
